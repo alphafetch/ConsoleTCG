d8bf523dd082dff6a4cc89a43a2b46f8ffedbceb
theTacocat88 <eliasglick1@gmail.com>
2026-07-26 12:46:17 -0400

    feat: disallow specific inputs in clean_input(), add quit() function to help_func.py, styles.py for global text styling

--- util/help_func.py
+++ util/help_func.py
@@ -1,16 +1,19 @@
 from colorama import Fore, Style
 
 import sys
+from . import styles
 
-def clean_input(prompt: str, requirements: list[str]) -> str:
+def clean_input(prompt: str, requirements: list[str], disallowances: list[str]) -> str:
     '''
     Get output that (if the wrong input is entered) resets the line with a customizable error message.
 
     :param prompt: The prompt used for the input
     :param requirements: What the user input must satisfy to continue
+    :param disallowances: What the user input cannot be
 
     :type prompt: str
     :type requirements: list[str]
+    :type disallowances: list[str]
 
     :return: Returns the correct user input, iterates until it returns this.
     :rtype: str
@@ -41,18 +44,29 @@ def clean_input(prompt: str, requirements: list[str]) -> str:
         # *     can be accepted or not.
         if u_input in requirements:
             # [!] User input passes requirements, clear error slot
-            # [!] and return the input to the function call
-            sys.stdout.write("\n\033[K\033[1A\r")
-            sys.stdout.flush()
-            return u_input
+            if u_input in disallowances:
+                # [;] Input was disallowed
+                has_error = True
+                error = styles.format_style(f"Error: No user data. Please select a different option.", "error")
+
+                # Print a newline so the error message goes underneath the input
+                sys.stdout.write("\n")
+            else:
+                # [!] Return user input - passed all tests
+                sys.stdout.write("\n\033[K\033[1A\r")
+                sys.stdout.flush()
+                return u_input
         else:
             # [;] The loop failed to meet the requirements
             # [;] Set the error message for the next loop iteration
             has_error = True
-            error = Fore.RED + Style.BRIGHT + f"Error: `{u_input}` is not a valid response. Please try again."
+            error = styles.format_style(f"Error: `{u_input}` is not a valid response. Please try again.", "error")
             
             # Print a newline so the error message goes underneath the input
             sys.stdout.write("\n")
 
-def quit():
-    pass
+def quit() -> None:
+    '''
+    Exits with exit code 0.
+    '''
+    sys.exit(0)
---
+++ util/styles.py
@@ -0,0 +1,30 @@
+from colorama import Fore, Back, Style
+
+styles = {
+    "error": Fore.RED + Style.BRIGHT,
+    "success": Fore.GREEN + Style.BRIGHT,
+    "bold_cyan": Fore.CYAN + Style.BRIGHT
+}
+
+# Used to print a styled message
+def format_style(text: str, style: str):
+    '''
+    Return a styled message.
+
+    :param text: The text to join with the style
+    :param type: Types: error, success
+
+    :type text: str
+    :type style: str
+
+    :return: The joined text of the style and the text parameter
+    :rtype: str
+    '''
+
+    match style:
+        case "error":
+            return styles["error"] + text
+        case "success":
+            return styles["success"] + text
+        case _:
+            return text
