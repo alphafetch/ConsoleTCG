0e7670a6642e94265c0064834521a7a128c2cb9d
theTacocat88 <eliasglick1@gmail.com>
2026-07-27 11:30:55 -0400

    refactor: minor main menu changes

--- util/scr.py
+++ util/scr.py
@@ -22,9 +22,9 @@ def scr_main_menu() -> str:
     helper.clear()
 
     # Print the menu
+    print(styles.format_style("┌─────────────────────────┐", "bold_cyan"))
     print(styles.format_style("│ Welcome to Console TCG! │", "bold_cyan"))
-    print(styles.format_style("└─────────────────────────┘", "bold_cyan"))
-    print()
+    print(styles.format_style("└─────────────────────────┘\n", "bold_cyan"))
     print("1. Play Game")
     print("2. New Game (OVERWRITES)")
     print("3. View Collection")
@@ -35,10 +35,10 @@ def scr_main_menu() -> str:
     # Get user input and return it
     if os.path.exists(imp.user_data_toml):
         # [!] User has a save file
-        u_input = helper.clean_input("> ", ["1", "2", "3", "4"], [])
+        u_input = helper.clean_input("> ", ["1", "2", "3", "4"])
     else:
         # [^] User does not have a save file
-        u_input = helper.clean_input("> ", ["1", "2", "3", "4"], ["1", "3"])
+        u_input = helper.clean_input("> ", ["1", "2", "3", "4"], ["1", "3"], styles.format_style(f"Error: No user data. Please select a different option.", "error"))
 
     return u_input
 
