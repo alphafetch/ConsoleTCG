0f929944f78c916430b646980ac15e30e212bc52
theTacocat88 <eliasglick1@gmail.com>
2026-08-11 10:22:20 -0400

    fix: show health after a health increase by and AMR card

--- game/match.py
+++ game/match.py
@@ -291,7 +291,7 @@ def start_match(world: int, enemy: Opponent, number: int) -> bool:
                                 # 3a. Health Temp Inc
                                 case "health_temp_increase":
                                     u_health += value
-                                    print(f"Your health increased by {styles.format_style("+", "green")}{styles.format_style(str(value), "green")}{styles.clear_styles()}!")
+                                    print(f"Your health increased by {styles.format_style("+", "green")}{styles.format_style(str(value), "green")}{styles.clear_styles()}! (HP: {u_health})")
                                     time.sleep(1.5)
                                 case "defend_round":
                                     defense_u += value
@@ -428,7 +428,7 @@ def start_match(world: int, enemy: Opponent, number: int) -> bool:
                             # 3a. Health Temp Inc
                             case "health_temp_increase":
                                 op_health += value
-                                print(f"Enemy health increased by {styles.format_style("+", "green")}{styles.format_style(str(value), "green")}{styles.clear_styles()}!")
+                                print(f"Enemy health increased by {styles.format_style("+", "green")}{styles.format_style(str(value), "green")}{styles.clear_styles()}! (HP: {op_health})")
                                 time.sleep(1.5)
                             case "defend_round":
                                 defense_e += value
